a4a2365026863ffc1728ed4480faeb4ec5fff3cc
Casey Wilson <caswil10@gmail.com>
2020-07-06 20:44:45 -0400

    Built Recipe Controller -- untested

--- PantryProject_API/Controllers/RecipeController.cs
+++ PantryProject_API/Controllers/RecipeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PantryProject.Models.Recipe;
+using PantryProject_Services.MainObjectServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,45 @@ using System.Web.Http;
 
 namespace PantryProject_API.Controllers
 {
+    [RoutePrefix("api/Recipe")]
     public class RecipeController : ApiController
     {
+        private readonly RecipeService _recipeService = new RecipeService();
+
+        [HttpGet]
+        public IHttpActionResult GetAllRecipes()
+        {
+            return Ok(_recipeService.Get_Recipes());
+        }
+        [HttpGet]
+        public IHttpActionResult GetRecipeById(int id)
+        {
+            return Ok(_recipeService.Get_RecipeById(id));
+        }
+        [HttpPost]
+        public IHttpActionResult CreateRecipe(RecipeCreate model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!_recipeService.Create_Recipe(model))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult PutRecipe(RecipeEdit model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            if (!_recipeService.EditRecipe(model))
+                return InternalServerError();
+            return Ok();
+        }
+        [HttpDelete]
+        public IHttpActionResult Delete_PreparedItemById(int id)
+        {
+            if (!_recipeService.DeleteRecipe(id))
+                return InternalServerError();
+            return Ok();
+        }
     }
 }
